2539ba1b57f315faccdd7155a94cdebaf9643c17
Beytullah AZTEKİN <aztekinbeytullah@gmail.com>
2021-01-17 00:51:40 +0300

    Proje dosyası ekle.

---
+++ GriSeviyeliGoruntuler/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace GriSeviyeliGoruntuler
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnResimYukle = new System.Windows.Forms.Button();
+            this.btnGriYap = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnBinaryYap = new System.Windows.Forms.Button();
+            this.btnOtsuAlgoritmasiUygula = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnBlop = new System.Windows.Forms.Button();
+            this.btnDondur = new System.Windows.Forms.Button();
+            this.btnResimKlasoruSec = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(630, 531);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnResimYukle
+            // 
+            this.btnResimYukle.Location = new System.Drawing.Point(12, 12);
+            this.btnResimYukle.Name = "btnResimYukle";
+            this.btnResimYukle.Size = new System.Drawing.Size(75, 23);
+            this.btnResimYukle.TabIndex = 1;
+            this.btnResimYukle.Text = "Resim Yükle";
+            this.btnResimYukle.UseVisualStyleBackColor = true;
+            this.btnResimYukle.Click += new System.EventHandler(this.btnResimYukle_Click);
+            // 
+            // btnGriYap
+            // 
+            this.btnGriYap.Location = new System.Drawing.Point(93, 12);
+            this.btnGriYap.Name = "btnGriYap";
+            this.btnGriYap.Size = new System.Drawing.Size(75, 23);
+            this.btnGriYap.TabIndex = 1;
+            this.btnGriYap.Text = "Gri Yap";
+            this.btnGriYap.UseVisualStyleBackColor = true;
+            this.btnGriYap.Click += new System.EventHandler(this.btnGriYap_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnBinaryYap
+            // 
+            this.btnBinaryYap.Location = new System.Drawing.Point(174, 12);
+            this.btnBinaryYap.Name = "btnBinaryYap";
+            this.btnBinaryYap.Size = new System.Drawing.Size(75, 23);
+            this.btnBinaryYap.TabIndex = 2;
+            this.btnBinaryYap.Text = "Binary Yap";
+            this.btnBinaryYap.UseVisualStyleBackColor = true;
+            this.btnBinaryYap.Click += new System.EventHandler(this.btnBinaryYap_Click);
+            // 
+            // btnOtsuAlgoritmasiUygula
+            // 
+            this.btnOtsuAlgoritmasiUygula.Location = new System.Drawing.Point(255, 12);
+            this.btnOtsuAlgoritmasiUygula.Name = "btnOtsuAlgoritmasiUygula";
+            this.btnOtsuAlgoritmasiUygula.Size = new System.Drawing.Size(75, 23);
+            this.btnOtsuAlgoritmasiUygula.TabIndex = 2;
+            this.btnOtsuAlgoritmasiUygula.Text = "Otsu";
+            this.btnOtsuAlgoritmasiUygula.UseVisualStyleBackColor = true;
+            this.btnOtsuAlgoritmasiUygula.Click += new System.EventHandler(this.btnOtsuAlgoritmasiUygula_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox4.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(663, 531);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 0;
+            this.pictureBox4.TabStop = false;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox4);
+            this.splitContainer1.Size = new System.Drawing.Size(1301, 533);
+            this.splitContainer1.SplitterDistance = 632;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.btnBlop);
+            this.panel1.Controls.Add(this.btnDondur);
+            this.panel1.Controls.Add(this.btnOtsuAlgoritmasiUygula);
+            this.panel1.Controls.Add(this.btnResimKlasoruSec);
+            this.panel1.Controls.Add(this.btnResimYukle);
+            this.panel1.Controls.Add(this.btnGriYap);
+            this.panel1.Controls.Add(this.btnBinaryYap);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1301, 86);
+            this.panel1.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(1169, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 69);
+            this.listBox1.TabIndex = 3;
+            // 
+            // btnBlop
+            // 
+            this.btnBlop.Location = new System.Drawing.Point(438, 12);
+            this.btnBlop.Name = "btnBlop";
+            this.btnBlop.Size = new System.Drawing.Size(75, 23);
+            this.btnBlop.TabIndex = 2;
+            this.btnBlop.Text = "BlopBul";
+            this.btnBlop.UseVisualStyleBackColor = true;
+            // 
+            // btnDondur
+            // 
+            this.btnDondur.Location = new System.Drawing.Point(336, 12);
+            this.btnDondur.Name = "btnDondur";
+            this.btnDondur.Size = new System.Drawing.Size(75, 23);
+            this.btnDondur.TabIndex = 2;
+            this.btnDondur.Text = "Döndür";
+            this.btnDondur.UseVisualStyleBackColor = true;
+            this.btnDondur.Click += new System.EventHandler(this.btnDondur_Click);
+            // 
+            // btnResimKlasoruSec
+            // 
+            this.btnResimKlasoruSec.Location = new System.Drawing.Point(1088, 12);
+            this.btnResimKlasoruSec.Name = "btnResimKlasoruSec";
+            this.btnResimKlasoruSec.Size = new System.Drawing.Size(75, 23);
+            this.btnResimKlasoruSec.TabIndex = 1;
+            this.btnResimKlasoruSec.Text = "Resimleri Seç";
+            this.btnResimKlasoruSec.UseVisualStyleBackColor = true;
+            this.btnResimKlasoruSec.Click += new System.EventHandler(this.btnResimKlasoruSec_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.splitContainer1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 86);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1301, 533);
+            this.panel2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1301, 619);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnResimYukle;
+        private System.Windows.Forms.Button btnGriYap;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnBinaryYap;
+        private System.Windows.Forms.Button btnOtsuAlgoritmasiUygula;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnDondur;
+        private System.Windows.Forms.Button btnResimKlasoruSec;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnBlop;
+    }
+}
+
---
+++ GriSeviyeliGoruntuler/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using AForge.Math.Geometry;
+
+
+namespace GriSeviyeliGoruntuler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] dosyalar;
+        BackgroundWorker bg = new BackgroundWorker();
+
+        private void btnResimYukle_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+            {
+                return;
+            }
+            pictureBox1.ImageLocation = openFileDialog1.FileName;
+            //bg.DoWork += Bg_DoWork;
+            //bg.RunWorkerAsync();
+
+        }
+
+        private void Bg_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < dosyalar.Length - 1; i++)
+            {
+                pictureBox1.ImageLocation = dosyalar[i].ToString();
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+
+        private void btnGriYap_Click(object sender, EventArgs e)
+        {
+
+
+            Bitmap image = new Bitmap(pictureBox1.Image);
+            GrayscaleBT709 griResim = new GrayscaleBT709();
+            pictureBox4.Image = griResim.Apply((Bitmap)pictureBox1.Image);
+        }
+
+        private void btnBinaryYap_Click(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(pictureBox1.Image);
+            Threshold threshold = new Threshold();
+            pictureBox4.Image = threshold.Apply(Grayscale.CommonAlgorithms.BT709.Apply(image));
+        }
+
+        private void btnOtsuAlgoritmasiUygula_Click(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(pictureBox1.Image);
+            OtsuThreshold otsuFiltre = new OtsuThreshold();
+            //Bitmap filtreliResim = otsuFiltre.Apply(image.PixelFormat != PixelFormat.Format8bppIndexed ? Grayscale.CommonAlgorithms.BT709.Apply(image) : image);
+            Bitmap filtreliResim = otsuFiltre.Apply(Grayscale.CommonAlgorithms.BT709.Apply(image));
+            pictureBox4.Image = filtreliResim;
+            this.Text = "Threshold Değeri : " + otsuFiltre.ThresholdValue.ToString();
+        }
+
+        private void btnDondur_Click(object sender, EventArgs e)
+        {
+
+            DocumentSkewChecker skewChecker = new DocumentSkewChecker();
+            double angle = skewChecker.GetSkewAngle((Bitmap) pictureBox4.Image);
+            RotateBilinear rotationFilter = new RotateBilinear(-angle);
+            rotationFilter.FillColor = Color.White;
+            // rotate image applying the filter
+            Bitmap rotatedImage = rotationFilter.Apply((Bitmap)pictureBox4.Image);
+            pictureBox1.Image = rotatedImage;
+
+        }
+
+        private void btnResimKlasoruSec_Click(object sender, EventArgs e)
+        {
+
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+
+                listBox1.Items.Clear();
+                dosyalar = Directory.GetFiles(fbd.SelectedPath);
+                foreach (string dosya in dosyalar)
+                {
+                    if (dosya.EndsWith(".jpg") || dosya.EndsWith(".jpeg"))
+                        listBox1.Items.Add(dosya);
+                }
+            }
+        }
+
+        List<IntPoint> leftPoints, rightPoints, edgePoints;
+
+        /*
+        private void btnBlop_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap = (Bitmap)pictureBox1.Image;
+            // lock image
+            BitmapData bitmapData = bitmap.LockBits(
+                new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                ImageLockMode.ReadWrite, bitmap.PixelFormat);
+
+            // step 1 - turn background to black
+            ColorFiltering colorFilter = new ColorFiltering();
+
+            colorFilter.Red = new IntRange(0, 64);
+            colorFilter.Green = new IntRange(0, 64);
+            colorFilter.Blue = new IntRange(0, 64);
+            colorFilter.FillOutsideRange = false;
+            colorFilter.ApplyInPlace(bitmapData);
+
+
+
+
+            // step 2 - locating objects
+            BlobCounter blobCounter = new BlobCounter();
+
+            blobCounter.FilterBlobs = true;
+            blobCounter.MinHeight = 5;
+            blobCounter.MinWidth = 5;
+
+            blobCounter.ProcessImage(bitmapData);
+            Blob[] blobs = blobCounter.GetObjectsInformation();
+            bitmap.UnlockBits(bitmapData);
+
+
+
+            // step 3 - check objects' type and highlight
+            SimpleShapeChecker shapeChecker = new SimpleShapeChecker();
+            
+
+            Graphics g = Graphics.FromImage(bitmap);
+            Pen redPen = new Pen(Color.Red, 2);
+            Pen yellowPen = new Pen(Color.Yellow, 2);
+            Pen greenPen = new Pen(Color.Green, 2);
+            Pen bluePen = new Pen(Color.Blue, 2);
+
+            for (int i = 0, n = blobs.Length; i < n; i++)
+            {
+                List<IntPoint> edgePoints =
+                    blobCounter.GetBlobsEdgePoints(blobs[i]);
+
+                AForge.Point center;
+                float radius;
+
+
+                if (shapeChecker.IsCircle(edgePoints,out center, out radius))
+                {
+                    g.DrawEllipse(yellowPen,
+                        (float)(center.X - radius), (float)(center.Y - radius),
+                        (float)(radius * 2), (float)(radius * 2));
+                }
+                else
+                {
+                    List<IntPoint> corners;
+                    if (shapeChecker.IsQuadrilateral(edgePoints, out corners))
+                    {
+                        if (shapeChecker.CheckPolygonSubType(corners) ==
+                            PolygonSubType.Rectangle)
+                        {
+                            g.DrawPolygon(greenPen, ToPointsArray(corners));
+                        }
+                        else
+                        {
+                            g.DrawPolygon(bluePen, ToPointsArray(corners));
+                        }
+                    }
+                    else
+                    {
+                        corners = PointsCloud.FindQuadrilateralCorners(edgePoints);
+                        g.DrawPolygon(redPen, ToPointsArray(corners));
+                    }
+                }
+            }
+
+            redPen.Dispose();
+            greenPen.Dispose();
+            bluePen.Dispose();
+            yellowPen.Dispose();
+            g.Dispose();
+
+        } 
+
+        */
+    
+    }
+}
